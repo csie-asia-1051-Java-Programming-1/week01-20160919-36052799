ffdd31a60fd8edad20437e1b99e61d8a79f45c3c
36052799 <105021035@live.asia.edu.tw>
2016-12-26 14:53:31 +0800

    Update hw01_1050210XX.java

--- week01_20160919/src/hw/hw01_1050210XX.java
+++ week01_20160919/src/hw/hw01_1050210XX.java
@@ -1,9 +1,13 @@
 package hw;
-
-public class hw01_1050210XX {
+import java.util.Scanner;
+public class hw01_105021035 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		System.out.println(Math.ceil(Math.log(n)/Math.log(2))); 
+		
 
 	}
 
