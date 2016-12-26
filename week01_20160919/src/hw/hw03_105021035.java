9c39fbbaa3afb4e222e42ede7b786061f39afe81
36052799 <105021035@live.asia.edu.tw>
2016-12-26 14:59:31 +0800

    Update and rename hw03_1050210XX.java to hw03_105021035.java

---
+++ week01_20160919/src/hw/hw03_105021035.java
@@ -0,0 +1,30 @@
+package hw;
+import java.util.Scanner;
+public class hw03_1050210XX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		float a=ran.nextFloat();
+		float b=ran.nextFloat();
+		float c=ran.nextFloat();
+		float d=ran.nextFloat();
+		float e=ran.nextFloat();
+		float f=ran.nextFloat();
+		int one=Math.round(a);
+		int two=Math.round(b);
+		int three=Math.round(c);
+		int four=Math.round(d);
+		int five=Math.round(e);
+		int six=Math.round(f);
+		int z=Math.min(one,two);
+		int y=Math.min(three,four);
+		int x=Math.min(five,six);
+		int w=Math.min(z,y);
+		int v=Math.min(y,x);
+		int u=Math.min(w,v);
+		System.out.println(u);
+
+	}
+
+}
