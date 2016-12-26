f7a61e59c2a89493019710065b9fe84d7ae6367e
36052799 <105021035@live.asia.edu.tw>
2016-12-26 14:57:57 +0800

    Update and rename hw02_1050210XX.java to hw02_105021035.java

---
+++ week01_20160919/src/hw/hw02_105021035.java
@@ -0,0 +1,21 @@
+package hw;
+import java.util.Random;
+import java.util.Scanner;
+
+public class hw02_1050210XX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random ran = new Random();
+		Scanner scn = new Scanner(System.in);
+		int r=scr.nextInt();
+		int a=ran.nextInt(r)+1;
+		int b=ran.nextInt(r)+1;
+		int c=ran.nextInt(r)+1;
+		int d=ran.nextInt(r)+1;
+		int e=ran.nextInt(r)+1;
+		int f=ran.nextInt(r)+1;
+		System.out.print(a+"\n"+ b+"\n"+ c+"\n"+ d+"\n"+ e+"\n"+ f+"\n");
+         }
+
+}
