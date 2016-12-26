634c858f6fc44b3910474c58ef7319f64ff90606
36052799 <105021035@live.asia.edu.tw>
2016-12-26 15:02:08 +0800

    Update and rename hw04_1050210XX.java to hw04_105021035.java

---
+++ week01_20160919/src/hw/hw04_105021035.java
@@ -0,0 +1,13 @@
+package hw;
+import java.util.Scanner;
+public class hw04_1050210XX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+		int a = Scanner.nextInt;
+		int b = Scanner.nextInt;
+		System.out.println(Math.pow(a,(double)1/b));
+	}
+
+}
